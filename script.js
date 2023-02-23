6348d278b31a6e408ee4232488ea0b4d1f820e03
mizucode <mizurisysthensis3@gmail.com>
2023-02-23 12:15:55 +0700

    music player

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ accordionHeaders.forEach((accordionHeader) => {
         }
     });
 });
+
+const musicPlayer = document.querySelector(".music-player");
+const audio = musicPlayer.querySelector("audio");
+const playPauseBtn = musicPlayer.querySelector(".play-pause-btn");
+const stopBtn = musicPlayer.querySelector(".stop-btn");
+const volumeSlider = musicPlayer.querySelector(".volume-slider");
+
+playPauseBtn.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.textContent = "Pause";
+    } else {
+        audio.pause();
+        playPauseBtn.textContent = "Play";
+    }
+});
+
+stopBtn.addEventListener("click", () => {
+    audio.pause();
+    audio.currentTime = 0;
+    playPauseBtn.textContent = "Play";
+});
+
+volumeSlider.addEventListener("input", () => {
+    audio.volume = volumeSlider.value / 100;
+});
